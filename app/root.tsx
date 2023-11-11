1a5ade6035f9fccd1b21fc427ffb4afbb4290942
andrewshpak <andrii.shpak@outlook.com>
2023-11-11 18:05:57 +0200

    remove vite

--- app/root.tsx
+++ app/root.tsx
@@ -1,25 +1,36 @@
-import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration,} from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 import {NextUIProvider} from "@nextui-org/react";
-import "./tailwind.css";
+import styles from "./tailwind.css";
 
+export const links: LinksFunction = () => [
+   { rel: "stylesheet", href: styles }
+];
 
 export default function App() {
-    return (
-        <html lang="en">
-        <head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <Meta/>
-            <Links/>
-        </head>
-        <body>
-        <NextUIProvider>
-            <Outlet/>
-            <ScrollRestoration/>
-            <LiveReload/>
-            <Scripts/>
-        </NextUIProvider>
-        </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+      <NextUIProvider>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </NextUIProvider>
+      </body>
+    </html>
+  );
 }
